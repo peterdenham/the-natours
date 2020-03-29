ea2ac41fdf4e3efa98704a293c49cb142e2cc476
Peter Denham <peter.denham@gmail.com>
2020-03-29 02:17:39 -0400

    testing middleware

--- starter/app.js
+++ starter/app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log('hello middleware');
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
