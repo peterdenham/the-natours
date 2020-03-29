dd72471980e9040dcb3cd3093b77b9e84b4ed28e
Peter Denham <peter.denham@gmail.com>
2020-03-29 02:19:43 -0400

    request time middleware

--- starter/app.js
+++ starter/app.js
@@ -9,14 +9,21 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     results: tours.length,
+    time: req.requestTime,
     data: {
       tours: tours,
     },
